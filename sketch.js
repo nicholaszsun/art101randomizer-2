d167a62e3514e0c0c131a8d7372f7089fbdbc5b5
nicholasz-sun <nicholas.sun@sjsu.edu>
2022-10-11 16:33:50 -0700

    cleaned up certain text

--- sketch.js
+++ sketch.js
@@ -26,21 +26,6 @@ function setup() {
   createCanvas(600, 600)
   background(230)
 
-//randomIndex = int(random(pcparts.length))
-//Command to call specific array of objects
-  //console.log(pcparts[2].description);
-
-//Command to call random object in the array
-/////Remeber to use "int" to make the randomizer pull using whole numbers instead of fractions
-///////////console.log(pcparts[int(random(pcparts.length))]);
-//Command to call just the name in the array
-///////console.log(pcparts[randomIndex].name);
-
-//Command to display the array on the page instead of the console
-//text(pcparts[randomIndex].name, 100, 100);
-
-//Command to delete the recently called array so it is not called again
-//pcparts.splice(randomIndex, 1);
 }
 
 function draw() {
